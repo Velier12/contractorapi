3e2b1eec797908963314e623b80f56273fe45a36
fiarr4ik <fiarr4ik@gmail.com>
2024-07-03 11:42:50 +0700

    init commit

---
+++ src/main/java/ru/fiarr4ik/contractorapi/ContractorapiApplication.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.contractorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fiarr4ik/contractorapi/ContractorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.contractorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
