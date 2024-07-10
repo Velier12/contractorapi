9aaef48264e1694d20761a05ef94a1010787e0d7
fiarr4ik <fiarr4ik@gmail.com>
2024-07-10 16:56:14 +0700

    feat: добавил openapi swagger

--- src/main/java/ru/fiarr4ik/contractorapi/ContractorapiApplication.java
+++ src/main/java/ru/fiarr4ik/contractorapi/ContractorapiApplication.java
@@ -1,9 +1,24 @@
 package ru.fiarr4ik.contractorapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ContractorApi",
+                description = "Spring boot REST API (contractor) для работы с контрагентами финтех приложения",
+                version = "0.0.1",
+                contact = @Contact(
+                        name = "Fiarr4ikDev",
+                        email = "fiarr4ikdev@gmail.com"
+                )
+        )
+)
 public class ContractorapiApplication {
 
     public static void main(String[] args) {
